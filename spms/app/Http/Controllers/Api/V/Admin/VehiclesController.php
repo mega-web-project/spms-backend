351c3f54f22c721977159103da31e12bee35341c
ZorinGab <princeafrim1@gmail.com>
2026-01-16 08:03:15 +0000

    VehiclesModels

---
+++ spms/app/Http/Controllers/Api/V/Admin/VehiclesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\V\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VehiclesController extends Controller
+{
+    //
+}
---
+++ spms/app/Models/Vehicles.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vehicles extends Model
+{
+    //
+
+    protected $fillable = [
+        'full_name',
+        'email',
+        'password',
+        'status',
+        'role'
+    ];
+    
+}
